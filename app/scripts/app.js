87572e69315e0612e3dc46f217cc84d9b791bcb9
Manuel Lopez <jmlopez.rod@gmail.com>
2015-02-02 21:47:39 -0600

    Testing Changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/location', {
+        templateUrl: 'views/location.html',
+        controller: 'LocationCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
